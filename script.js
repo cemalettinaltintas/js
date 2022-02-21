44e6b86ae7ccbb2b281b8f3fb32e8794265108d6
Cemalettin Altıntaş <cemalettinaltintas@gmail.com>
2022-02-21 07:52:55 +0300

    ilk ders

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("merhaba")
+console.log(1234)
+console.log(true);
+console.log([1,2,3,4]);
+console.error('hata mesajı');
+console.warn("uyarı")
+
+// tek satırlık yorum satırı
+
+/*
+yorum satırları
+yorum satırları
+yorum satırları
+*/
+
+console.clear();
